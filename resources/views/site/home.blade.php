0ef69495943cbf1e54fd74f83a2d19928c400630
rafaelpolicher <110497809+RafaelPolicher@users.noreply.github.com>
2023-04-28 18:38:23 -0300

    estruturas de controle

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -9,5 +9,43 @@
 {{ isset($nome) ? 'assim se faz operador ternario' : 'nao existe' }}<br>
 
 {{$nomeee ?? 'coloca um valor padrao numa variavel inexistente'}}
+<br><br>
 
+{{--estruturas de controle--}}
+@if ($nome == 'Rafael')
+    true
+@else
+    false
+@endif
+<br><br>
+
+
+@switch($idade)
+    @case(27)
+        idade esta ok
+        @break
+    @case(29)
+        idade esta errada
+        @break
+    @default
+        default
+@endswitch
+<br><br>
+
+@isset($nome)
+    existe
+@endisset
+
+@empty($nome)
+    esta vazia
+@endempty
+<br><br>
+
+@auth
+    existe usuario autenticado
+@endauth
+
+@guest
+    nao existe usuario autenticado
+@endguest
 @endsection
