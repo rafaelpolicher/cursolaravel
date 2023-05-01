f29681bd97ac335bfa4ed6107b1730c3fbe6def0
rafaelpolicher <110497809+RafaelPolicher@users.noreply.github.com>
2023-05-01 17:48:38 -0300

    darryldecode/cart

---
+++ app/Http/Controllers/CarrinhoController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CarrinhoController extends Controller
+{
+    public function carrinhoLista(){
+        $itens = \Cart::getContent();
+        dd($itens);
+    }
+}
