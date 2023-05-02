f6daa022ac9ff67dc8f9f9a66bf9509598c83157
rafaelpolicher <110497809+RafaelPolicher@users.noreply.github.com>
2023-05-02 15:22:16 -0300

    middlewares

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
+
+    /*public function __construct(){
+        $this->middleware('auth')->only('index');//except(['index', 'contato'])
+    }middleware esta apenas na rota em web.php porem tambem é possivel fazer desta forma*/
+
     public function index(){
         return view('/admin/dashboard');
     }
--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,11 @@ Route::resource('produtos', ProdutoController::class);
 Route::get('/', [SiteController::class, 'index'])->name('site.index');
 Route::get('/produto/{slug}', [SiteController::class, 'details'])->name('site.details');
 Route::get('/categoria/{id}', [SiteController::class, 'categoria'])->name('site.categoria');
+
 Route::get('/carrinho', [CarrinhoController::class, 'carrinhoLista'])->name('site.carrinho');
 Route::post('/carrinho', [CarrinhoController::class, 'adicionaCarrinho'])->name('site.addcarrinho');
 
-Route::get('/admin/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+Route::get('/admin/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard')->middleware('auth');
 
 Route::view('/login', 'login.form')->name('login.form');
 Route::post('/auth', [LoginController::class, 'auth'])->name('login.auth');
